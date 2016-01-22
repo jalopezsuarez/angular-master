9affe711c8f27707c9e664e9df2aad31fd17e175
jalopezsuarez <jalopezsuarez@gmail.com>
2016-01-22 11:04:56 +0100

    angularjs meteo archetype

---
+++ angular-master/app/models/services/meteo/forecast.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+ngApplicationFactory.factory('StandingsService', StandingsService);
+function StandingsService($http)
+{
+	var standingsService = {};
+
+	standingsService.realizarPeticion = function()
+	{
+		var resultado = $http({
+			method : 'JSONP',
+			url : 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+		});
+		return resultado;
+	}
+
+	return standingsService;
+}
+
+// http://api.openweathermap.org/data/2.5/forecast?q=Almeria&appid=2de143494c0b295cca9337e1e96b00e0
